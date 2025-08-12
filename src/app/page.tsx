cc953fd199fdd33cdcbff3386ff2aa2b29249ea9
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-08-12 19:02:35 +0200

    removed blank div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 
       <ActionButton />
 
-      <div className="h-100 bg-gray-800"></div>
+      <div className="h-100 bg-gray-800/0"></div>
 
       <h1 className="text-7xl h-10 text-white /50 max-w-screen overflow-hidden">WWWWWWWWWWWWWWW</h1>
       <h1 className="text-7xl h-10 text-white /50 max-w-screen overflow-hidden">WWWWWWWWWWWWWWW</h1>
