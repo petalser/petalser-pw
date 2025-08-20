dedd420d6270c562e46986586fbb6c4894e7f4ec
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-08-20 21:55:43 +0200

    new components consumed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
-import ActionButton from "@/components/ActionButton";
 import Hero from "@/components/Hero";
+import ActionButton from "@/components/ActionButton";
+import CreedSection from "@/components/CreedSection";
+import BlurryLettersWrapper from "@/components/BlurryLettersWrapper";
+import AboutStatesSection from "@/components/AboutStatesSection";
 
 export default function Home() {
   return (
     <div className=" flex flex-col min-h-screen">
       <Hero />
-
       <ActionButton />
+      <BlurryLettersWrapper>
+        <CreedSection />
+        <AboutStatesSection />
+      </BlurryLettersWrapper>
 
       <div className="h-100 bg-gray-800/0"></div>
 
