e6616a24d4bd9637f9db5f18d952722dd5b43532
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-08-20 22:33:30 +0200

    layout updates

--- src/components/CreedSection.tsx
+++ src/components/CreedSection.tsx
@@ -1,7 +1,7 @@
 export default function CreedSection() {
     return (
         <section className="w-full bg-gradient-to-b from-background via-background to-transparent ">
-            <h2 className="font-medium w-4/5 big:w-3/5 mx-auto py-5 big:py-16 text-xl big:text-3xl">
+            <h2 className="font-medium w-4/5 big:w-3/5 mx-auto py-5 big:py-16 mb-10 text-xl big:text-3xl">
                 Simple<span className="font-light"> yet </span>beautiful,
                 <br />
                 easy to use<span className="font-light"> regardless of complexity</span>
