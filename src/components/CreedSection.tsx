bf20a2a48591a434065297492493032d71ed2f20
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-08-20 21:55:24 +0200

    Init

---
+++ src/components/CreedSection.tsx
@@ -0,0 +1,12 @@
+export default function CreedSection() {
+    return (
+        <section className="w-full bg-gradient-to-b from-background via-background to-transparent ">
+            <h2 className="font-medium w-4/5 big:w-3/5 mx-auto py-5 big:py-16 text-xl big:text-3xl">
+                Simple<span className="font-light"> yet </span>beautiful,
+                <br />
+                easy to use<span className="font-light"> regardless of complexity</span>
+            </h2>
+        </section>
+
+    )
+}
