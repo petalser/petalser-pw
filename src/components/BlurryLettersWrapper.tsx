e211a0707b24573655e09a044dcbe68e6277e2be
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-08-20 13:10:03 +0200

    section decoration component

---
+++ src/components/BlurryLettersWrapper.tsx
@@ -0,0 +1,39 @@
+"use client"
+import { ReactNode } from "react";
+import { motion, useScroll, useTransform } from "motion/react";
+import { useRef } from "react";
+
+type BlurryLettersWrapperProps = {
+    children: ReactNode
+}
+
+const CHARS = "nZkXoFqvLaQyRjPWhsdUeTGlrVfMIZcptbDkSgYwHOmCjNRuXxAqvKfLzJoEyPThBdWnCmUiRsVgYkXaQmFjLpZcDoWrNtHbUzSxVyKeCfMiGpJlQdXaOwRnTuZsVkHpCbYjMgLfQrNaWiXtUzPvKhDcSbYoJmGlQfRnXwTaUzKvHpCdSiMbYoLgQnFrXwTtUaZpKhDvCmSnZkXoFqvLaQyRjPWhsdUeTGlrVfMIZcptbDkSgYwHOmCjNRuXxAqvKfLzJoEyPThBdWnCmUiRsVgYkXaQmFjLpZcDoWrNtHbUzSxVyKeCfMiGpJlQdXaOwRnTuZsVkHpCbYjMgLfQrNaWiXtUzPvKhDcSbYoJmGlQfRnXwTaUzKvHpCdSiMbYoLgQnFrXwTtUaZpKhDvCmSnZkXoFqvLaQyRjPWhsdUeTGlrVfMIZcptbDkSgYwHOmCjNRuXxAqvKfLzJoEyPThBdWnCmUiRsVgYkXaQmFjLpZcDoWrNtHbUzSxVyKeCfMiGpJlQdXaOwRnTuZsVkHpCbYjMgLfQrNaWiXtUzPvKhDcSbYoJmGlQfRnXwTaUzKvHpCdSiMbYoLgQnFrXwTtUaZpKhDvCmSnZkXoFqvLaQyRjPWhsdUeTGlrVfMIZcptbDkSgYwHOmCjNRuXxAqvKfLzJoEyPThBdWnCmUiRsVgYkXaQmFjLpZcDoWrNtHbUzSxVyKeCfMiGpJlQdXaOwRnTuZsVkHpCbYjMgLfQrNaWiXtUzPvKhDcSbYoJmGlQfRnXwTaUzKvHpCdSiMbYoLgQnFrXwTtUaZpKhDvCmSnZkXoFqvLaQyRjPWhsdUeTGlrVfMIZcptbDkSgYwHOmCjNRuXxAqvKfLzJoEyPThBdWnCmUiRsVgYkXaQmFjLpZcDoWrNtHbUzSxVyKeCfMiGpJlQdXaOwRnTuZsVkHpCbYjMgLfQrNaWiXtUzPvKhDcSbYoJmGlQfRnXwTaUzKvHpCdSiMbYoLgQnFrXwTtUaZpKhDvCmS"
+
+export default function BlurryLettersWrapper({ children }: BlurryLettersWrapperProps) {
+    const containerRef = useRef<HTMLDivElement | null>(null)
+    const { scrollYProgress } = useScroll({
+        target: containerRef,
+        offset: ["start end", "end start"]
+    })
+
+    //"y" positions for different layers
+    //of background decoration
+    const y1 = useTransform(scrollYProgress, velocity => velocity * 500)
+    const y2 = useTransform(scrollYProgress, velocity => (velocity * 1500) - 300)
+
+    return (
+        <div ref={containerRef} className="w-full relative overflow-hidden">
+
+            {children}
+
+            <motion.div style={{ y: y1 }} className="flex absolute blur-[10px] z-[-3] -translate-y-24 inset-0 flex-wrap gap-2 justify-around align-middle text-white/50 text-8xl">
+                {CHARS.split('').map((char: string, i: number) => <span className="" key={i}>{char}</span>)}
+            </motion.div>
+
+            <motion.div style={{ y: y2 }} className="absolute blur-[10px] z-[-3] left-0 -translate-y-24 flex flex-wrap gap-4 justify-around align-middle text-white/50 text-6xl">
+                {CHARS.split('').map((char: string, i: number) => <span className="" key={i}>{char}</span>)}
+            </motion.div>
+
+        </div >
+    )
+}
