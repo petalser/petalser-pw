0f65153ba7f257a75944f280b03256222fce8d39
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-09-02 15:02:28 +0200

    Updated styles (gradient shift)

--- src/components/CreedSection.tsx
+++ src/components/CreedSection.tsx
@@ -1,6 +1,6 @@
 export default function CreedSection() {
     return (
-        <section className="w-full bg-gradient-to-b from-background via-background to-transparent ">
+        <section className="w-full bg-gradient-to-b from-background from-[80%] to-transparent to-[100%]">
             <h2 className="font-medium w-4/5 big:w-3/5 mx-auto py-5 big:py-16 mb-10 text-xl big:text-3xl">
                 Simple<span className="font-light"> yet </span>beautiful,
                 <br />
