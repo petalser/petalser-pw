4e4dd23a5820c71f6cf892aff3637a9dec11a854
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-09-02 14:55:42 +0200

    Updated bg styling to make it more sharp

--- src/components/BlurryLettersWrapper.tsx
+++ src/components/BlurryLettersWrapper.tsx
@@ -26,11 +26,11 @@ export default function BlurryLettersWrapper({ children }: BlurryLettersWrapperP
 
             {children}
 
-            <motion.div style={{ y: y1 }} className="flex absolute blur-[10px] z-[-3] -translate-y-24 inset-0 flex-wrap gap-2 justify-around align-middle text-white/50 text-8xl">
+            <motion.div style={{ y: y1 }} className="flex absolute blur-[3px] z-[-3] -translate-y-24 inset-0 flex-wrap gap-2 justify-around align-middle text-white/70 text-8xl">
                 {CHARS.split('').map((char: string, i: number) => <span className="" key={i}>{char}</span>)}
             </motion.div>
 
-            <motion.div style={{ y: y2 }} className="absolute blur-[10px] z-[-3] left-0 -translate-y-24 flex flex-wrap gap-4 justify-around align-middle text-white/50 text-6xl">
+            <motion.div style={{ y: y2 }} className="absolute inset-0 blur-[5px] z-[-4] left-0 -translate-y-24 flex flex-wrap gap-4 justify-around align-middle text-white/70 text-6xl">
                 {CHARS.split('').map((char: string, i: number) => <span className="" key={i}>{char}</span>)}
             </motion.div>
 
