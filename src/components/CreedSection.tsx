a98643d8b79b69045356f90ddde57fb2c33ea6a3
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-09-08 14:16:23 +0200

    Minor layout update

--- src/components/CreedSection.tsx
+++ src/components/CreedSection.tsx
@@ -1,12 +1,17 @@
+import SmoothenedCard from "./SmothenedCard"
+
 export default function CreedSection() {
     return (
-        <section className="w-full bg-gradient-to-b from-background from-[80%] to-transparent to-[100%]">
-            <h2 className="font-medium w-4/5 big:w-3/5 mx-auto py-5 big:py-16 mb-10 text-xl big:text-3xl">
-                Simple<span className="font-light"> yet </span>beautiful,
-                <br />
-                easy to use<span className="font-light"> regardless of complexity</span>
-            </h2>
-        </section>
+        <>
+            <section className="w-full bg-background pb-10">
+                <h2 className="font-medium w-4/5 big:w-[48%] mx-auto py-5 big:py-16 text-xl big:text-3xl">
+                    Simple<span className="font-light"> yet </span>beautiful,
+                    <br />
+                    easy to use<span className="font-light"> regardless of complexity</span>
+                </h2>
+            </section>
+            <SmoothenedCard.Bottom />
+        </>
 
     )
 }
